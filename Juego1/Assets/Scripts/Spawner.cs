cce332057824f71c8f47ed145d62d682401dad39
IsaiasRO <Isaiasilvestre@gmail.com>
2017-09-14 15:34:30 -0500

    juego1
    ya lo subi

---
+++ Juego1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	[SerializeField] GameObject galleta;
+
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			float rndX = Random.Range (-6f, 6f);
+			float rndY = Random.Range (-4f, 4f);
+			Instantiate (galleta, new Vector3(rndX,rndY,0), Quaternion.identity);	
+		}
+	}
+	void Update () {
+		
+	}
+}
