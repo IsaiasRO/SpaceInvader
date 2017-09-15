10408931d825b245a93b471b6bf51eed9cc69888
IsaiasRO <31941527+IsaiasRO@users.noreply.github.com>
2017-09-14 21:49:17 -0500

    correccion de disparo
    dispara sin utilizar rigbody,

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-	[SerializeField] float speed = 5;
+	private float speed = 5;
 
 	void Start () {
-		Rigidbody2D rb = GetComponent<Rigidbody2D>();
-		rb.velocity  = new Vector2 (0, 1) * speed;
+		//Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		//rb.velocity  = new Vector2 (0, 1) * speed;
 	}
 	
 
 	void Update () {
-		
+		transform.Translate (Vector3.up * speed * Time.deltaTime);
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,8 @@ public class GameManager : MonoBehaviour {
 	[SerializeField] GameObject Alien;
 
 	void Awake(){
-		for (int i = -0; i < 10; i++) 
+		for (int i = -5; i < 5; i++) 
 		{
-			for(int i = -0)
 			Instantiate	(Alien,new Vector2 (i,0),Quaternion.identity);	
 		}
 	}
